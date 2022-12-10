1dc377b41f81d24bc0edfc8900977a54c917adc3
Patrick Organ <patrick@permafrost.dev>
2022-12-09 23:22:05 -0500

    wip

--- .github/scripts/get-matrix.sh
+++ .github/scripts/get-matrix.sh
@@ -1,4 +1,5 @@
-platforms=(linux/amd64 linux/arm64)
+platforms=(linux/amd64)
+# linux/arm64
 tags=('latest' 'jammy,2204' 'focal,2004')
 matrix=()
 for platform in "${platforms[@]}"; do
@@ -7,25 +8,14 @@ for platform in "${platforms[@]}"; do
       base="ubuntu:22.04"
       file="jammy"
     elif [[ "$tag" =~ jammy ]]; then
-      if [ "$platform" = "linux/386" ]; then
-        continue
-      fi
       base="ubuntu:22.04"
       file="jammy"
     elif [[ "$tag" =~ focal ]]; then
-      if [ "$platform" = "linux/386" ]; then
-        continue
-      fi
       base="ubuntu:20.04"
       file="focal"
     fi
-    if [[ "$tag" =~ slim ]]; then
-      build_args="type=slim"
-    else
-      build_args="type=full"
-    fi
-    matrix+=("{\"tags\": \"$tag\", \"base\": \"$base\", \"build_args\": \"$build_args\", \"file\": \"$file\"}")
+    build_args="type=full"
+    matrix+=("{\"platform\": \"$platform\", \"tags\": \"$tag\", \"base\": \"$base\", \"build_args\": \"$build_args\", \"file\": \"$file\"}")
   done
 done
 echo "::set-output name=matrix::{\"include\":[$(echo "${matrix[@]}" | sed -e 's|} {|}, {|g')]}"
-echo "BUILD_PLATFORM=$platform" >> $GITHUB_ENV
